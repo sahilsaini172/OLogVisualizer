be9ed139b9eae6e14312fc81d75409e1a4b36c7e
Sahil Saini <sahilsainimani8398@gmail.com>
2025-11-11 14:58:05 +0530

    Bubblesort play and init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import SideBar from "../components/Sidebar";
 
@@ -51,36 +51,69 @@ function Section2() {
 
 function Section3() {
   const n = 10;
-  const array = [];
+  const [array, setArray] = useState([]);
+  const [sortedArray, setSortedArray] = useState([]);
 
-  function randomFillArray(array) {
+  function init() {
+    const newArray = [];
     for (let i = 0; i < n; i++) {
-      array[i] = Math.random();
+      newArray.push(Math.random());
     }
+    setSortedArray([]);
+    setArray(newArray);
   }
 
-  function bubbleSort(arr) {
+  function play() {
+    bubbleSort(array);
+  }
+
+  function bubbleSort(array) {
+    const newArray = [...array];
+    let swapped;
     do {
-      var swapped = false;
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i - 1 > arr[i]]) {
-          swapped = true[(arr[i - 1], (arr[i] = [arr[i], arr[i - 1]]))];
+      swapped = false;
+      for (let i = 0; i < newArray.length; i++) {
+        if (newArray[i + 1] > newArray[i]) {
+          swapped = true;
+          [newArray[i + 1], newArray[i]] = [newArray[i], newArray[i + 1]];
         }
       }
     } while (swapped);
+    setSortedArray(newArray);
   }
 
   function showArray(array) {
-    randomFillArray(array);
-    return array.map((value, index) => (
-      <Bar key={index} height={value * 100 + "%"} backgroundColor={"white"} />
-    ));
+    return array.map((value, index) => {
+      return (
+        <Bar key={index} height={value * 100 + "%"} backgroundColor={"white"} />
+      );
+    });
   }
+
+  useEffect(() => {
+    init();
+  }, []);
 
   return (
     <section className="p-6 flex items-center justify-center">
-      <div className="flex h-[200px] items-end gap-6 justify-end">
-        {showArray(array)}
+      <div className="flex flex-col ">
+        <div className="flex h-[200px] items-end gap-6 justify-end">
+          {sortedArray.length > 0 ? showArray(sortedArray) : showArray(array)}
+        </div>
+        <div className="flex items-center gap-4 mt-5">
+          <button
+            onClick={init}
+            className="px-4 bg-orange-500/25 py-2 rounded-lg text-orange-400"
+          >
+            init
+          </button>
+          <button
+            onClick={play}
+            className="px-4 bg-orange-500/25 py-2 rounded-lg text-orange-400 disabled:opacity-50"
+          >
+            play
+          </button>
+        </div>
       </div>
     </section>
   );
@@ -89,7 +122,7 @@ function Section3() {
 function Bar(props) {
   return (
     <div
-      key={props.key}
+      key={props.index}
       style={{ height: props.height, backgroundColor: props.backgroundColor }}
       className="w-2.5"
     ></div>
